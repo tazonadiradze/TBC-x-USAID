27d73039fe8a4b14999dbbc798ea5d0b948aeb88
tazo01~ <nadiradzetazo123@gmail.com>
2024-01-27 18:29:17 +0400

    finish course page

--- tbc-usaid/src/components/common/course/course.tsx
+++ tbc-usaid/src/components/common/course/course.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import Image from "../image";
-
+import "./course.css";
 interface ExpandableProps {
   title: string;
   status: string;
@@ -9,12 +9,15 @@ interface ExpandableProps {
 
 const Course: FC<ExpandableProps> = ({ image, title, status }) => {
   return (
-    <div>
-      <div>
+    <div className="course-wrapper">
+      <div className="image-text-wrapper">
         <div>{image && <Image src={image} alt="string" />}</div>
-        <div>
-          <div>{title}</div>
-          <div>{status}</div>
+        <div className="text-button-wrapper">
+          <div className="text-wrapper">
+            <div>{title}</div>
+            <div>{status}</div>
+          </div>
+          <div>კურსის დეტალები</div>
         </div>
       </div>
     </div>
--- tbc-usaid/src/components/coursesPanel/index.tsx
+++ tbc-usaid/src/components/coursesPanel/index.tsx
@@ -1,62 +1,74 @@
 import Course from "../common/course/course";
-import img from "../../assets/backgroundimage.png";
-import secimage from "../../assets/sssdad.png";
-const dummydata = [
+import "./index.css";
+import FirstImage from "../../assets/courseImages/firstImage.png";
+import SecondImage from "../../assets/courseImages/secondImage.png";
+import ThirdImage from "../../assets//courseImages/thirdImage.png";
+import FourthImage from "../../assets//courseImages/fourthImage.png";
+import FifthImage from "../../assets//courseImages/fifthImage.png";
+import SixthImage from "../../assets//courseImages/sixthImage.png";
+import SeventhImage from "../../assets//courseImages/seventhImage.png";
+import EighthImage from "../../assets//courseImages/eighthImage.png";
+import NinethImage from "../../assets/courseImages/ninethImage.png";
+const Courses = [
   {
     title: "iOS Development",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${FirstImage}`,
   },
   {
     title: "React",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${secimage}`,
+    img: `${SecondImage}`,
   },
   {
     title: "Intro to Python",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${ThirdImage}`,
   },
   {
     title: "Advanced Python",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${FourthImage}`,
   },
   {
     title: " Advanced Cybersecurity Course",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${FifthImage}`,
   },
   {
     title: "ToT - Training of Trainers",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${SixthImage}`,
   },
   {
     title: "Blockchain",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${SeventhImage}`,
   },
   {
     title: "DevOps",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${EighthImage}`,
   },
   {
     title: "Information Security Governance",
     status: "რეგისტრაცია დასრულებულია",
-    img: `${img}`,
+    img: `${NinethImage}`,
   },
 ];
 
 function CoursesPanel() {
-  return (
-    <div>
-      {dummydata.map(({ title, status, img }) => {
-        return <Course title={title} status={status} image={img} />;
-      })}
-    </div>
-  );
+  {
+    return (
+      <div className="courses-panel-wrapper">
+        <div className="courses-wrapper">
+          {Courses.map(({ title, status, img }) => {
+            return <Course title={title} status={status} image={img} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CoursesPanel;
